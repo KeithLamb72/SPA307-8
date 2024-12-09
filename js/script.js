93c7e48ba378d8a2d2888dac39a1be442e57d07d
Keith Lamb <keithianlamb@gmail.com>
2024-12-09 12:07:46 -0600

    Set up files and directories compared to READMe.md File

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/* Use an array to store tasks.
+On add.html form submission, validate input using regex and if good, push a new task into the array (you can also store them in localStorage if you want to persist across pages).
+On index.html, loop through the tasks array to display each task in a table row.
+Show some event handling, like a button click event to remove a task or highlight a row.
+ */
